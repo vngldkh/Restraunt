39e718d636464e8e208e11ece67c3ce8eb2a88c4
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 20:54:06 +0300

    Converted into class

--- src/manager/MenuDish.java
+++ src/manager/MenuDish.java
@@ -1,4 +1,28 @@
 package manager;
 
-public record MenuDish(int menu_dish_id, int menu_dish_card, int menu_dish_price, Boolean menu_dish_active) {
+public class MenuDish {
+    int menu_dish_id;
+    int menu_dish_card;
+    int menu_dish_price;
+    boolean menu_dish_active;
+
+    public MenuDish(int menu_dish_id, int menu_dish_card,
+                    int menu_dish_price, boolean menu_dish_active) {
+        this.menu_dish_id = menu_dish_id;
+        this.menu_dish_card = menu_dish_card;
+        this.menu_dish_price = menu_dish_price;
+        this.menu_dish_active = menu_dish_active;
+    }
+
+    public int getMenuDishId() {
+        return menu_dish_id;
+    }
+
+    public boolean getMenuDishActive() {
+        return menu_dish_active;
+    }
+
+    public void makeUnavailable() {
+        menu_dish_active = false;
+    }
 }
