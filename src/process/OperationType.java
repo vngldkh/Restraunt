b94f074ac0937133635f0088ac41c474108e7c53
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 14:52:04 +0300

    Has been converted into record

--- src/process/OperationType.java
+++ src/process/OperationType.java
@@ -1,6 +1,5 @@
 package process;
 
-public class OperationType {
-    int oper_type_id;
-    String oper_type_name;
+// unnecessary
+public record OperationType (int oper_type_id, String oper_type_name) {
 }
