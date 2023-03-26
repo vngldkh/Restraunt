05afe32c9d5823d7d2e17512b5c204d45c386511
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 09:35:31 +0300

    Class Handbook created

---
+++ src/manager/Handbook.java
@@ -0,0 +1,11 @@
+package manager;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Handbook {
+    ConcurrentHashMap<Integer, DishCard> handbook;
+
+    Handbook(DishCard[] dishCards) {
+
+    }
+}
