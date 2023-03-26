0eaacfc9fd7b52804e19c617bfdaade1d79522b8
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 09:38:26 +0300

    Handbook class implemented

--- src/manager/Handbook.java
+++ src/manager/Handbook.java
@@ -3,9 +3,15 @@ package manager;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Handbook {
-    ConcurrentHashMap<Integer, DishCard> handbook;
+    ConcurrentHashMap<Integer, DishCard> handbook = new ConcurrentHashMap<>();
 
     Handbook(DishCard[] dishCards) {
+        for (var dishCard : dishCards) {
+            handbook.put(dishCard.card_id(), dishCard);
+        }
+    }
 
+    DishCard getDishCard(int cardId) {
+        return handbook.get(cardId);
     }
 }
