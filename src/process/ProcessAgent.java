2ddbfe01979c196d21afaa39cb037215a54c012e
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 14:52:57 +0300

    ProcessAgent implemeted

--- src/process/ProcessAgent.java
+++ src/process/ProcessAgent.java
@@ -1,7 +1,30 @@
 package process;
 
+import manager.DishCard;
+import simulation.Simulation;
+
 import java.util.ArrayList;
 
-public class ProcessAgent {
+public class ProcessAgent implements Runnable {
+    Simulation simulation;
+    DishCard dishCard;
     ArrayList<OperationAgent> operationAgents;
+
+    public ProcessAgent(Simulation simulation, DishCard dishCard) {
+        this.simulation = simulation;
+        this.dishCard = dishCard;
+    }
+
+    @Override
+    public void run() {
+        Manual manual = simulation.getRestaurant().getManager().ProvideManual();
+        OperationType[] operationTypes = dishCard.operations();
+        operationAgents = new ArrayList<>();
+        for (var operationType : operationTypes) {
+            Operation operation = manual.getOperation(operationType.oper_type_id());
+            OperationAgent operationAgent = new OperationAgent(simulation, operation);
+            operationAgents.add(operationAgent);
+            simulation.execute(operationAgent);
+        }
+    }
 }
