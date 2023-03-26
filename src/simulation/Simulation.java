9c3f9c9f8c3fa65c198fd7b123c50e147bec4d4f
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 16:58:44 +0300

    Synchronization instruments added

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -3,12 +3,14 @@ package simulation;
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Simulation {
-    private final LocalDateTime currentDateTime;
+    private LocalDateTime currentDateTime;
     private final Restaurant restaurant;
     private final ExecutorService threadPool;
+    private int subscribers;
+    private int respondCount;
+    public final Object monitor = new Object();
 
     Simulation() {
         currentDateTime = LocalDateTime.now();
@@ -26,6 +28,32 @@ public class Simulation {
 
     public void execute(Runnable task) {
         threadPool.execute(task);
+    }
+
+    public synchronized void Subscribe() {
+        ++subscribers;
+    }
+
+    public synchronized void Unsubscribe() {
+        --subscribers;
+    }
+
+    public synchronized void Respond() {
+        ++respondCount;
+    }
+
+    public void run() {
+        synchronized (monitor) {
+            do {
+                try {
+                    wait(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } while (respondCount != subscribers);
+            respondCount = 0;
+            currentDateTime = currentDateTime.plusSeconds(1);
+        }
     }
 }
 
