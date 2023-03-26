20afc712b42b10fc39f5eb7db0203081ab3c8c2f
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 16:56:59 +0300

    Managing of cookers and equipments

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -1,14 +1,19 @@
 package manager;
 
+import process.CookerAgent;
+import process.EquipmentAgent;
 import process.Manual;
 import simulation.Simulation;
+import storage.StorageAgent;
 import visitor.VisitorAgent;
 
 import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Manager {
     Simulation simulation;
-    ArrayList<OrderAgent> orders;
+    CopyOnWriteArrayList<OrderAgent> orders = new CopyOnWriteArrayList<>();
+    CopyOnWriteArrayList<CookerAgent> availableCookers = new CopyOnWriteArrayList<>();
 
     public Manager(Simulation simulation) {
         this.simulation = simulation;
@@ -28,5 +33,33 @@ public class Manager {
 
     public void AcceptOrder(VisitorAgent visitor, ArrayList<MenuDish> order) {
         orders.add(new OrderAgent(simulation, visitor, order));
+    }
+
+    public synchronized CookerAgent GetAvailableExecutor() {
+        if (availableCookers.isEmpty()) {
+            return null;
+        }
+        var executor = availableCookers.get(0);
+        availableCookers.remove(0);
+        executor.Engage();
+        return executor;
+    }
+
+    public synchronized void FreeExecutor(CookerAgent cookerAgent) {
+        cookerAgent.Free();
+        availableCookers.add(cookerAgent);
+    }
+
+    public synchronized EquipmentAgent ReserveEquipment(int id) {
+        var equipments = simulation.getRestaurant().getEquipments();
+        if (!equipments.containsKey(id)) {
+            return null;
+        }
+        var equipment = equipments.get(id);
+        if (!equipment.isAvailable()) {
+            return null;
+        }
+        equipment.Use();
+        return equipment;
     }
 }
