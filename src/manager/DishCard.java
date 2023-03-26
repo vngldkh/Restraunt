17458e0e089f36c2c68c3105071043fc0fb9a8dd
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 09:35:21 +0300

    Have been converted into records

--- src/manager/DishCard.java
+++ src/manager/DishCard.java
@@ -5,10 +5,7 @@ import process.OperationType;
 import java.util.Date;
 import java.util.ArrayList;
 
-public class DishCard {
-    int card_id;
-    String dish_name;
-    Date card_time;
-    int equip_type;
-    ArrayList<OperationType> operations;
+public record DishCard(int card_id, String dish_name,
+                       Date card_time, int equip_type,
+                       ArrayList<OperationType> operations) {
 }
--- src/process/Operation.java
+++ src/process/Operation.java
@@ -1,8 +1,6 @@
 package process;
 
-public class Operation {
-    int oper_type;
-    double oper_time;
-    int oper_async_point;
-    OperProduct[] oper_products;
+public record Operation(int oper_type, int equip_type,
+                        double oper_time, int oper_async_point,
+                        OperProduct[] oper_products) {
 }
