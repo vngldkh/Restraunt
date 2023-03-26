8da3ce1294e21126fd21cbe6cba5ec32fedd78e4
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 16:57:28 +0300

    Converted into record

--- src/process/Equipment.java
+++ src/process/Equipment.java
@@ -1,16 +1,4 @@
 package process;
 
-public class Equipment {
-    int equip_type;
-    String equip_name;
-    Boolean equip_active;
-
-    public Equipment(int equip_type,
-                       String equip_name,
-                       Boolean equip_active) {
-
-        this.equip_type = equip_type;
-        this.equip_name = equip_name;
-        this.equip_active = equip_active;
-    }
+public record Equipment (int equip_type, String equip_name, boolean equip_active) {
 }
