988fb77f567b7bf1abb9b70514865b0256057fe5
vngldkh <idgladkikh@edu.hse.ru>
2023-03-26 20:51:44 +0300

    OrderAgent fully implemented

--- src/manager/OrderAgent.java
+++ src/manager/OrderAgent.java
@@ -2,10 +2,11 @@ package manager;
 
 import process.ProcessAgent;
 import simulation.Simulation;
-import storage.StorageAgent;
 import visitor.VisitorAgent;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 
@@ -14,7 +15,10 @@ public class OrderAgent implements Runnable {
     visitor.VisitorAgent customer;
     ArrayList<MenuDish> orderedDishes;
     ArrayList<DishCard> dishCards;
-    ArrayList<ProcessAgent> processAgents;
+    ArrayList<Future<?>> processes;
+    private LocalDateTime timeStart;
+    private LocalDateTime timeEnd;
+    boolean done = false;
 
     OrderAgent(Simulation simulation, VisitorAgent customer, ArrayList<MenuDish> orderedDishes) {
         this.customer = customer;
@@ -25,14 +29,32 @@ public class OrderAgent implements Runnable {
     @Override
     public void run() {
         dishCards = new ArrayList<>();
-        Handbook handbook = simulation.getRestaurant().getManager().ProvideHandBook();
-        processAgents = new ArrayList<>();
+        Handbook handbook = simulation.getRestaurant().getManager().provideHandBook();
+        processes = new ArrayList<>();
+        timeStart = simulation.getCurrentTime();
         for (var menuDish : orderedDishes) {
-            var dishCard = handbook.getDishCard(menuDish.menu_dish_card());
+            var dishCard = handbook.getDishCard(menuDish.getMenuDishId());
             dishCards.add(dishCard);
-            var process = new ProcessAgent(simulation, dishCard);
-            processAgents.add(process);
-            simulation.execute(process);
+            var process = new ProcessAgent(simulation, dishCard, menuDish.getMenuDishId());
+            processes.add(simulation.submit(process));
         }
+        for (var future : processes) {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        timeEnd = simulation.getCurrentTime();
+        done = true;
+        log();
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void log() {
+        // TODO
     }
 }
